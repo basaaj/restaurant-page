3d7f1a307381662774deb48f25304d0debfe9dcd
j <93398891+basaaj@users.noreply.github.com>
2024-06-24 16:11:12 -0500

    Fix layout and style of homepage

--- src/home.js
+++ src/home.js
@@ -1,24 +1,25 @@
-import image from './img/cafe.jpg';
+import bg from './img/home.jpg';
 
 export function loadHome() {
     const contentDiv = document.querySelector('#content');
-
-    const headline = document.createElement('div');
-    headline.classList.add('headline');
-    const txt = document.createElement('div');
-    txt.innerHTML = `<h1>Have a sweet little treat.</h1>
-                     <h3>You deserve it. Stop by Bing Cafe today.</h3>
-                    <p>Insert text.</p>`;
-    headline.appendChild(txt);
     
     const homeImg = document.createElement('img');
-    homeImg.setAttribute('src', image);
+    homeImg.setAttribute('src', bg);
     homeImg.setAttribute('id', 'homeImg');
-    headline.appendChild(homeImg);
+    contentDiv.appendChild(homeImg);
 
-    contentDiv.appendChild(headline);
+    const homeDiv = document.createElement('div');
+    homeDiv.setAttribute('id', '#homeDiv');
+    homeDiv.innerHTML = `<h1>Bing Cafe</h1>
+                         <h3>Have a sweet little treat. You deserve it.</h3>
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris laoreet, justo nec rhoncus ullamcorper, 
+                         orci erat ullamcorper orci, ac blandit nisl mauris ornare justo. Integer vehicula, magna sit amet tincidunt sollicitudin, 
+                         lorem neque suscipit ante, eget hendrerit turpis sem aliquet est. Donec leo lorem, pellentesque at tempus et, dignissim nec eros. 
+                         Etiam semper enim sed nulla eleifend, sit amet luctus tortor molestie. Pellentesque non lectus dictum, rutrum dolor id, consectetur ante. 
+                         Pellentesque euismod eros vel augue aliquet, eu consequat ex efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>`;
+
+    contentDiv.appendChild(homeDiv);
 
     const homeTab = document.querySelector('#home');
     homeTab.classList.add('active');
 }
-
